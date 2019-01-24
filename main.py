05f074ecea2b014896978d8313fa5692d4463e04
aparna <aparnakothur@gmail.com>
2019-01-23 21:11:26 -0600

    modified main.py

--- main.py
+++ main.py
@@ -18,29 +18,17 @@ form="""
                 form {{
 
                     background-color: #eee;
-
                     padding: 20px;
-
                     margin: 0 auto;
-
                     width: 540px;
-
                     font: 16px sans-serif;
-
                     border-radius: 10px;
-
                 }}
-
                 textarea {{
-
                     margin: 10px 0;
-
                     width: 540px;
-
                     height: 120px;
-
                 }}
-
             </style>
 
         </head>
@@ -53,7 +41,7 @@ form="""
                 <label><br><br>
                 <textarea name="text">{0}</textarea>
                 </label>
-                <input type="submit" value="Submit " />
+                <input type="submit" value="Submit">
             </form>
         </body>
     </html>
@@ -61,15 +49,15 @@ form="""
 @app.route("/")
 def index():
     #return form.__format__('')
-    return form
-app.run()
+    return form.format('')
+#app.run()
 
 @app.route("/", methods=['POST'])
 def encrypt():
     local_rot = int(request.form['rot'])
     local_text = request.form['text']
-    enc_string = rotate_string(local_text, local_rot)
-    return enc_string
-    #return form.format(rotate_string(local_text, local_rot))
-    
+    #enc_string = rotate_string(local_text, local_rot)
+    #return form.format(enc_string)
+    return form.format(rotate_string(local_text, local_rot))
+app.run()    
 
